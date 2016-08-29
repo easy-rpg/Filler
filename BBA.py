f29c4f041786c10becd9853320c9837775b750d1
RodrigoNdeC <rodrigondec@gmail.com>
2016-08-29 19:52:37 -0300

    adicionado valores de BBA

---
+++ BBA.py
@@ -0,0 +1,19 @@
+import abc
+
+class BBA_Boa(object):
+	"""docstring for Barbaro"""
+	__metaclass__ = abc.ABCMeta
+	valores = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+	@abc.abstractmethod
+	def __str__(self):
+		raise NotImplementedError('users must define __str__ to use this base class')
+
+class BBA_Ruim(object):
+	"""docstring for Barbaro"""
+	__metaclass__ = abc.ABCMeta
+	valores = [0, 1, 2, 3, 3, 4, 5, 6, 6, 7, 8, 9, 9, 10, 11, 12, 12, 13, 14, 15]
+
+	@abc.abstractmethod
+	def __str__(self):
+		raise NotImplementedError('users must define __str__ to use this base class')
