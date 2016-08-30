769ab832ad434aedf7308c8889aca64331ac7b8e
RodrigoNdeC <rodrigondec@gmail.com>
2016-08-30 00:52:04 -0300

    personagens funcionando

---
+++ Personagem.py
@@ -0,0 +1,87 @@
+#coding=utf-8
+from Classes import *
+from Situacao import Situacao
+from Writer import Writer
+import json
+import types
+
+def load_personagem(caminho):
+	with open(caminho) as json_data:
+	    d = json.load(json_data)
+	    # print d
+
+	personagem = Personagem(d['nome'], d['atributos']['for'], d['atributos']['dex'], d['atributos']['con'], d['atributos']['int'], d['atributos']['sab'], d['atributos']['car'])
+
+	for classe in d['classes']:
+		modul = __import__("Classes")
+		class_ = getattr(modul, classe)
+		instance = class_(d['classes'][classe])
+		personagem.set_class(instance)
+
+	return personagem
+
+class Personagem(object):
+	"""docstring for Personagem"""
+	def __init__(self, nome, forca, destreza, constituicao, inteligencia, sabedoria, carisma):
+		super(Personagem, self).__init__()
+		self.nome = nome
+		self.classes = {}
+		self.atributos = {}
+		self.atributos['for'] = forca
+		self.atributos['dex'] = destreza
+		self.atributos['con'] = constituicao
+		self.atributos['int'] = inteligencia
+		self.atributos['sab'] = sabedoria
+		self.atributos['car'] = carisma
+
+		self.atualizar()
+
+	def set_class(self, obj):
+		self.classes[type(obj).__name__] = obj
+		self.atualizar()
+		return Situacao(True, "Classe adicionada com sucesso")
+
+	def atualizar(self):
+		self.update_personagem()
+		self.save()
+
+	def update_personagem(self):
+		self.life = 0
+		self.nivel = 0
+		for classe in self.classes:
+			self.nivel += self.classes[classe].nivel
+			self.life += self.classes[classe].nivel*(self.classes[classe].dv + (self.atributos['con']-10)/2)
+
+	def save(self):
+		Writer(self)
+
+	def __str__(self):
+		string = "Nome do personagem: " + self.nome
+		string += "\n"
+		string += "lvl: " + str(self.nivel)
+		string += "\t"
+		string += "HP: " + str(self.life)
+		string += "\n"
+		if bool(self.classes):
+			string += "classes: "
+			for classe in self.classes:
+				string += classe + " lvl: " + str(self.classes[classe].nivel) + ""
+				string += " | "
+			string += "\n"
+		string += "Atributos: "
+		for atributo in self.atributos:
+		 	string += atributo + ": " + str(self.atributos[atributo])
+		 	string += "\t"
+		string += "\n"
+
+		return string
+
+
+# assis = Personagem('Assis', 17, 14, 20, 12, 14, 10)
+# assis.set_class(Barbaro(3))
+# assis.set_class(Guerreiro(4))
+# # print assis
+# # assis.save()
+
+# personagem = load_personagem("personagens/Assis.rf")
+# print personagem
